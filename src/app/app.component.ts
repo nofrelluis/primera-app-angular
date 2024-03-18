11b96b7306308cb01b6bac5e980ddb8a037265b3
nofrelluis <nofrelluis@gmail.com>
2024-03-18 19:05:19 +0100

    classe 36

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
+//selector: '[app-root]', seleccionar com a element a html
+//selector: '.app-root' seleccionar per classe,
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AnyCatcher } from 'rxjs/internal/AnyCatcher';
 
 @Component({
   selector: 'app-servers',
-  template: `
-  <app-server></app-server>
-  <app-server></app-server>`,
+  templateUrl: './servers.component.html',
   styleUrl: './servers.component.css'
 })
-export class ServersComponent {
+export class ServersComponent implements OnInit {
+  allowNewServer: boolean=false;
+  serverCreationStatus: string = 'This server was not created'
+  serverName = 'TestServr'
+  
+  constructor(){
+    setTimeout(()=>{
+      this.allowNewServer = true
+    },2000);
+  }
 
+  ngOnInit(): void{
+      
+  }
+
+  OnCreateServer(){
+    this.serverCreationStatus='This server was created! Name is' + this.serverName;
+  }
+
+  OnUpdateServerName(event: Event){
+    this.serverName=(<HTMLInputElement>event.target).value
+  }
 }
